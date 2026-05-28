8045ddf67415ad7d2426342dec1f63415da34337
Samuel Pearce <sam@aepearce.com>
2026-05-29 00:36:00 +0200

    Started overhauling Spacies

--- spacies/src/main.c
+++ spacies/src/main.c
@@ -1,65 +1,96 @@
 //	
 //		SPACE INVADERS
 //	
-//	Reimplemented using Raylib
+//	Recreated using Raylib
 //	
+
+
+//	
+//	Includes
+//	
+
 #include <raylib.h>
-#include <raymath.h>
-#include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <time.h>
 
 #include "../include/beep.h"
 #include "../include/track.h"
-#include "../include/sprite.h"
 
-#define FRAMERATE 60
-#define ANIM_SPEED 0.500f
+
+//	
+//	Macro Constants
+//	
+
+// Game Definitions
+#define FRAMERATE		60		// FPS
+#define TICKRATE		4		// TPS
+#define PLAYER_SPEED	250.0f	// px/s
+#define BULLET_SPEED	600.0f	// px/s
+#define MAX_BULLETS		32
+#define PADDING			50.0f	// px
 
 // Sounds
 #define SND_WAVE WAV_SQUARE
 
 // Inputs
-#define KEY_LEFT 263
-#define KEY_RIGHT 262
-#define KEY_FULLSCREEN 70
-#define KEY_THEME_1 49
-#define KEY_THEME_2 50
-#define KEY_THEME_3 51
-#define KEY_THEME_4 52
-#define KEY_THEME_5 53
+#define INPUT_LEFT			KEY_LEFT
+#define INPUT_RIGHT			KEY_RIGHT
+#define INPUT_FIRE			KEY_SPACE
+#define INPUT_FULLSCREEN	KEY_F
+#define INPUT_THEME_1		KEY_ONE
+#define INPUT_THEME_2		KEY_TWO
+#define INPUT_THEME_3		KEY_THREE
+#define INPUT_THEME_4		KEY_FOUR
+#define INPUT_THEME_5		KEY_FIVE
 
-// Game Constants
-#define PLAYER_SPEED 8
 
-// Function Declarations
-void reset_ball(Vector2 *pos, Vector2 *vel);
-void draw_score(int x, int n);
-void play_jingle();
+//	
+//	Type Definitions
+//	
+
+typedef struct {
+	uint8_t type;	// 0x00 = Player, 0x01 = Alien
+	int pos_x;
+	int pos_y;
+	int vel_y;
+} Bullet;
+
+
+//
+//	Function Declarations
+//
+
+void spawn_bullet(int x, int y, uint8_t type);
+void remove_bullet(int i);
 void print_help();
 
-// Globals
+//	
+//	Globals
+//
+
 static float g_pause_movement = 0.0f;	// How long to pause movement for
 static int g_colour_theme = 0;
+Bullet g_bullet_buf[MAX_BULLETS];
+int g_bullet_count = 0;
 
 const Color g_colour_themes[5][4] = {
-//		Background	Ball		Paddles		Score
+//			Background		Player			Aliens			Text
 	{	BLACK,		WHITE,		WHITE,		WHITE	},
 	{	BLACK,		ORANGE,		ORANGE,		ORANGE	},
 	{	DARKBLUE,	BLUE,		BLUE,		BLUE	},
 	{	BLACK,		RED,		GREEN,		WHITE	},
 	{	WHITE,		BLACK,		BLACK,		BLACK	},
 };
+
 #define CLR_BACKGROUND (g_colour_themes[g_colour_theme][0])
-#define CLR_BALL (g_colour_themes[g_colour_theme][1])
-#define CLR_PADDLE (g_colour_themes[g_colour_theme][2])
-#define CLR_DIGIT (g_colour_themes[g_colour_theme][3])
+#define CLR_PLAYER (g_colour_themes[g_colour_theme][1])
+#define CLR_ALIENS (g_colour_themes[g_colour_theme][2])
+#define CLR_TEXT (g_colour_themes[g_colour_theme][3])
 
-extern const uint8_t _binary_resources_spritesheet_bin_start[];
-extern const uint8_t _binary_resources_spritesheet_bin_end[];
 
+//	
+//	Main Function
+//	
 
 int main(int argc, char *argv[]) {
 
@@ -79,7 +110,7 @@ int main(int argc, char *argv[]) {
 	}
 
 	// Initialise
-	//SetTraceLogLevel(LOG_WARNING);
+	//SetTraceLogLevel(LOG_WARNING);	// TODO: Uncomment for PROD
 	SetRandomSeed(time(NULL));
 	InitWindow(1000, 800,
 		"Game Canon - Space Invaders"
@@ -90,113 +121,132 @@ int main(int argc, char *argv[]) {
 	SND_init();
 	SND_loadpreset(SND_WAVE);
 
-	// Set up game objects
-	SPR_Sprite game_sprites[] = {
-		{
-			.rect = {
-				GetRenderWidth() / 2.0, GetRenderHeight() - 80.0,
-				13, 8,
-			},
-			.vel = { 0.0, 0.0 },
-			.clr = CLR_PADDLE,
-			.frame_count = 1,
-			.frame_index = 0,
-			.spritesheet_origin = { 0, 0 },
-		},{
-			.rect = {
-				10, 10,
-				11, 8,
-			},
-			.vel = { 4.0, 0.0 },
-			.clr = GREEN,
-			.frame_count = 2,
-			.frame_index = 0,
-			.spritesheet_origin = { 0, 8 },
-		},{
-			.rect = {
-				10, 10 + 10 * PIXEL_SIZE,
-				12, 8,
-			},
-			.vel = { 4.0, 0.0 },
-			.clr = GREEN,
-			.frame_count = 2,
-			.frame_index = 0,
-			.spritesheet_origin = { 0, 16 },
-		},{
-			.rect = {
-				10, 10 + 20 * PIXEL_SIZE,
-				8, 8,
-			},
-			.vel = { 4.0, 0.0 },
-			.clr = GREEN,
-			.frame_count = 2,
-			.frame_index = 0,
-			.spritesheet_origin = { 0, 24 },
-		}
+	const Rectangle rec_bullet = {
+		.height = 40.0f,
+		.width = 15.0f,
 	};
-	#define SPRITE_COUNT (sizeof(game_sprites)/sizeof(SPR_Sprite))
 
-	SPR_Sprite *player_ship = &(game_sprites[0]);
-
-	Image img_spritesheet = LoadImageFromMemory(".png",
-		(uint8_t *) _binary_resources_spritesheet_bin_start,
-		_binary_resources_spritesheet_bin_end - _binary_resources_spritesheet_bin_start
-	);
-	SPR_load_spritesheet(img_spritesheet);
+	Rectangle player_ship = {
+		.height = 30.0f,
+		.width = 100.0f,
+	};
+	player_ship.x = GetScreenWidth() / 2.0f - player_ship.width / 2.0f;
+	player_ship.y = GetScreenHeight() - player_ship.height - PADDING;
 
 	// Main Loop
+	static int frame_counter = 0;
 	while (!WindowShouldClose()) {
 		TRK_update();
 
-		// DEBUG: Print key presses
-		int k = GetKeyPressed();
-		if (k != 0) printf("---> Key Pressed: %i\n", k); fflush(stdout);
+		// Handle Inputs
 
-		if (IsKeyPressed(KEY_FULLSCREEN)) ToggleFullscreen();
-		//if (IsKeyPressed(KEY_THEME_1)) g_colour_theme = 0;
-		//if (IsKeyPressed(KEY_THEME_2)) g_colour_theme = 1;
-		//if (IsKeyPressed(KEY_THEME_3)) g_colour_theme = 2;
-		//if (IsKeyPressed(KEY_THEME_4)) g_colour_theme = 3;
-		//if (IsKeyPressed(KEY_THEME_5)) g_colour_theme = 4;
+		//if (IsKeyPressed(INPUT_FULLSCREEN)) ToggleFullscreen();
+		if (IsKeyPressed(INPUT_THEME_1)) g_colour_theme = 0;
+		if (IsKeyPressed(INPUT_THEME_2)) g_colour_theme = 1;
+		if (IsKeyPressed(INPUT_THEME_3)) g_colour_theme = 2;
+		if (IsKeyPressed(INPUT_THEME_4)) g_colour_theme = 3;
+		if (IsKeyPressed(INPUT_THEME_5)) g_colour_theme = 4;
 
-		player_ship->vel.x = 0.0f;
-		if (IsKeyDown(KEY_LEFT)) player_ship->vel.x = -PLAYER_SPEED;
-		if (IsKeyDown(KEY_RIGHT)) player_ship->vel.x = PLAYER_SPEED;
+		float player_move = PLAYER_SPEED * GetFrameTime();
+		if (IsKeyDown(INPUT_LEFT) && player_ship.x > PADDING) {
+			player_ship.x -= player_move;
+		}
+		if (IsKeyDown(INPUT_RIGHT) && player_ship.x < GetScreenWidth() - PADDING - player_ship.width) {
+			player_ship.x += player_move;
+		}
 
-		// Update sprites
+		if (IsKeyPressed(INPUT_FIRE)) {
+			spawn_bullet(
+				player_ship.x + (player_ship.width / 2.0f) - (rec_bullet.width / 2.0f),
+				player_ship.y - (rec_bullet.height / 2.0f),
+				0x00
+			);
+		}
+
+
+		// Handle Ticks
+
 		static float anim_timer = 0.0f;
 		if (anim_timer <= 0.0f) {
-			for (int i=0; i<SPRITE_COUNT; i++) {
-				SPR_Sprite s = game_sprites[i];
-				game_sprites[i].rect.x += s.vel.x * PIXEL_SIZE;
-				game_sprites[i].rect.y += s.vel.y * PIXEL_SIZE;
+			spawn_bullet(
+				GetRandomValue(PADDING, GetScreenWidth() - PADDING),
+				GetRandomValue(PADDING, GetScreenHeight() - PADDING),
+				0x01
+			);
 
-				game_sprites[i].frame_index++; 
-				game_sprites[i].frame_index %= game_sprites[i].frame_count;
-			}
-			anim_timer = ANIM_SPEED;
+			anim_timer = 1.0f / TICKRATE;
 		} else {
 			anim_timer -= GetFrameTime();
 			if (anim_timer < 0.0f) anim_timer = 0.0f;
 		}
 
+		// Move Bullets
+		for (int i=0; i<g_bullet_count; i++) {
+			Bullet b = g_bullet_buf[i];
+			g_bullet_buf[i].pos_y += b.vel_y * GetFrameTime();
+
+			Rectangle r = rec_bullet;
+			r.x = b.pos_x;
+			r.y = b.pos_y;
+
+			// Check if we're off screen
+			if (b.pos_y >= GetScreenHeight()
+			   || b.pos_y <= 0.0f - rec_bullet.height
+			) {
+				remove_bullet(i);
+				i--; continue;
+			}
+
+			// TODO: Handle collisions
+		}
+
 		// Draw Elements
+
 		BeginDrawing();
 		ClearBackground(CLR_BACKGROUND);
 
-		for (int i=0; i<SPRITE_COUNT; i++) {
-			SPR_draw(game_sprites[i]);
+		// Draw Bullets
+		for (int i=0; i<g_bullet_count; i++) {
+			Bullet b = g_bullet_buf[i];
+			Rectangle r = rec_bullet;
+			r.x = b.pos_x;
+			r.y = b.pos_y;
+			Color c = CLR_PLAYER;
+			if (b.type == 0x01) c = CLR_ALIENS;
+			DrawRectangleRec(r, c);
 		}
+
+		// Draw Player
+		DrawRectangleRec(player_ship, CLR_PLAYER);
 
 		EndDrawing();
 	}
 
 	// Terminate
-	SPR_unload_spritesheet();
 	SND_terminate();
 	CloseWindow();
 
 	return EXIT_SUCCESS;
+}
+
+
+void spawn_bullet(int x, int y, uint8_t type) {
+	if (g_bullet_count >= MAX_BULLETS) return;
+
+	int bi = g_bullet_count++;
+	g_bullet_buf[bi].pos_x = x;
+	g_bullet_buf[bi].pos_y = y;
+	g_bullet_buf[bi].type = 0x00;
+
+	if (type == 0x01) g_bullet_buf[bi].vel_y = BULLET_SPEED;
+	else g_bullet_buf[bi].vel_y = -BULLET_SPEED;
+}
+
+void remove_bullet(int i) {
+	if (g_bullet_count <= 0) return;
+
+	g_bullet_count--;
+	g_bullet_buf[i] = g_bullet_buf[g_bullet_count];
 }
 
 void print_help() {
